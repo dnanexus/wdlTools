e2879dbdf15227aeea7ae1d1ef106d2ed284fea4
jdidion <github@didion.net>
2020-04-01 22:10:34 -0400

    Fix tests

--- src/test/scala/wdlTools/format/BaseTest.scala
+++ src/test/scala/wdlTools/format/BaseTest.scala
@@ -1,26 +1,31 @@
 package wdlTools.format
-
 import java.nio.file.{Path, Paths}
-import org.scalatest.{FlatSpec, Matchers}
 
-import wdlTools.syntax.ParseDocument
-import wdlTools.util.Options
-import wdlTools.util.URL
+import org.scalatest.{FlatSpec, Matchers}
+import wdlTools.formatter.WDL10Formatter
+import wdlTools.syntax.ParseAll
+import wdlTools.util.{Options, URL, Util}
 
 class BaseTest extends FlatSpec with Matchers {
   private lazy val conf = Options(antlr4Trace = false)
 
-  private def getWdlSource(fname: String): URL = {
-    val p: String = getClass.getResource(s"/format/${fname}").getPath
-    val path: Path = Paths.get(p)
-    URL(path.toString)
+  def getWdlPath(fname: String, subdir: String): Path = {
+    Paths.get(getClass.getResource(s"/format/${subdir}/${fname}").getPath)
+  }
+
+  private def getWdlURL(fname: String, subdir: String): URL = {
+    URL(getWdlPath(fname, subdir).toString)
   }
 
   it should "handle the runtime section correctly" in {
     val parser = ParseAll(conf)
-    val src = getWdlSource(fname = "simple.wdl", subdir = "after")
-    val doc = parser.apply(src)
+    val srcURL = getWdlURL(fname = "simple.wdl", subdir = "after")
+    val doc = parser.apply(srcURL)
     doc.version shouldBe "1.0"
+  }
+
+  def getWdlSource(fname: String, subdir: String): String = {
+    Util.readFromFile(getWdlPath(fname, subdir))
   }
 
   it should "reformat simple WDL" in {
