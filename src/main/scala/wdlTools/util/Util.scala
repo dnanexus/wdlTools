5bc72395efa7a02bbe1761aba0b6cd45bd2d79b1
jdidion <github@didion.net>
2020-04-09 11:36:27 -0400

    Remove code that isn't used yet

--- src/main/scala/wdlTools/util/Util.scala
+++ src/main/scala/wdlTools/util/Util.scala
@@ -220,30 +220,4 @@ object Util {
       case _ => a.toString
     }
   }
-
-  case class BiMap[X, Y](keys: Seq[X], values: Seq[Y]) {
-    require(keys.size == values.size, "no 1 to 1 relation")
-    private lazy val kvMap: Map[X, Y] = keys.zip(values).toMap
-    private lazy val vkMap: Map[Y, X] = values.zip(keys).toMap
-
-    def size: Int = keys.size
-
-    def fromKey(x: X): Y = kvMap(x)
-
-    def fromValue(y: Y): X = vkMap(y)
-
-    def filterKeys(p: X => Boolean): BiMap[X, Y] = {
-      BiMap.fromPairs(keys.zip(values).filter(item => p(item._1)))
-    }
-  }
-
-  object BiMap {
-    def fromPairs[X, Y](pairs: Seq[(X, Y)]): BiMap[X, Y] = {
-      BiMap(pairs.map(_._1), pairs.map(_._2))
-    }
-
-    def fromMap[X, Y](map: Map[X, Y]): BiMap[X, Y] = {
-      fromPairs(map.toVector)
-    }
-  }
 }
