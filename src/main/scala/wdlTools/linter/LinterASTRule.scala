c8567811294686a9ba5f126f54b186dba9d614a5
jdidion <github@didion.net>
2020-04-15 21:45:04 -0400

    Add addError to LinterASTRule

--- src/main/scala/wdlTools/linter/LinterASTRule.scala
+++ src/main/scala/wdlTools/linter/LinterASTRule.scala
@@ -1,7 +1,12 @@
 package wdlTools.linter
 
+import wdlTools.syntax.AbstractSyntax.Element
 import wdlTools.syntax.ASTVisitor
 
 import scala.collection.mutable
 
-class LinterASTRule(val id: String, errors: mutable.Buffer[LinterError]) extends ASTVisitor {}
+class LinterASTRule(val id: String, errors: mutable.Buffer[LinterError]) extends ASTVisitor {
+  protected def addError(element: Element): Unit = {
+    errors.append(LinterError(id, element.text))
+  }
+}
