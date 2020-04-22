03d24633269afdf49ebdb3629d5e1f6da17ce3d1
jdidion <github@didion.net>
2020-04-22 14:54:01 -0400

    Add separate formatMessage function in exceptions

--- src/main/scala/wdlTools/syntax/package.scala
+++ src/main/scala/wdlTools/syntax/package.scala
@@ -65,9 +65,11 @@ object TextSource {
 // Syntax error exception
 final class SyntaxException(message: String) extends Exception(message) {
   def this(msg: String, text: TextSource, docSourceURL: Option[URL] = None) = {
-    this(formatMessage(msg, text, docSourceURL))
+    this(SyntaxException.formatMessage(msg, text, docSourceURL))
   }
+}
 
+object SyntaxException {
   def formatMessage(msg: String, text: TextSource, docSourceURL: Option[URL]): String = {
     val urlPart = docSourceURL.map(url => s" in ${url.toString}").getOrElse("")
     s"${msg} at ${text}${urlPart}"
--- src/main/scala/wdlTools/typing/package.scala
+++ src/main/scala/wdlTools/typing/package.scala
@@ -7,9 +7,11 @@ import wdlTools.syntax.TextSource
 // Type error exception
 final class TypeException(message: String) extends Exception(message) {
   def this(msg: String, text: TextSource, docSourceURL: Option[URL] = None) = {
-    this(formatMessage(msg, text, docSourceURL))
+    this(TypeException.formatMessage(msg, text, docSourceURL))
   }
+}
 
+object TypeException {
   def formatMessage(msg: String, text: TextSource, docSourceURL: Option[URL]): String = {
     val urlPart = docSourceURL.map(url => s" in ${url.toString}").getOrElse("")
     s"${msg} at ${text}${urlPart}"
