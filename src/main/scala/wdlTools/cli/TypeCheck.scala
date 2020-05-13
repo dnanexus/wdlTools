6d2a3cb442f14abca7974740b95d9a7528f4918b
Ohad Rodeh <orodeh@users.noreply.github.com>
2020-05-13 10:41:43 -0700

    Handle WDL versions inside the types module
    The types Stdlib module handles handles variations in the standard library between WDL versions. Make it internal to type checking, so it isn't visible to the outside. This simplifies setting up and running the type-checker.

--- src/main/scala/wdlTools/cli/TypeCheck.scala
+++ src/main/scala/wdlTools/cli/TypeCheck.scala
@@ -1,7 +1,7 @@
 package wdlTools.cli
 
 import wdlTools.syntax.Parsers
-import wdlTools.types.{Context, Stdlib, TypeInfer}
+import wdlTools.types.{Context, TypeInfer}
 
 // TODO: nicely format errors, including JSON output
 case class TypeCheck(conf: WdlToolsConf) extends Command {
@@ -10,7 +10,7 @@ case class TypeCheck(conf: WdlToolsConf) extends Command {
     val opts = conf.check.getOptions
     require(opts.followImports)
     val parsers = Parsers(opts)
-    val checker = TypeInfer(Stdlib(opts))
+    val checker = TypeInfer(opts)
 
     parsers.getDocumentWalker[Context](url).walk { (_, doc, _) =>
       // TODO: rather than throw an exception as soon as a type error is encountered, accumulate all errors
