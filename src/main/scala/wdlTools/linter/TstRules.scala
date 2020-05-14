c488e6cf829afc8c6268aa00862da568e065b287
jdidion <github@didion.net>
2020-05-13 21:04:39 -0400

    Cleanup

--- src/main/scala/wdlTools/linter/TstRules.scala
+++ src/main/scala/wdlTools/linter/TstRules.scala
@@ -68,69 +68,74 @@ object TstRules {
         "read_json"
     )
 
-    private def isQuestionableCoercion(to: T, from: T): Boolean = {
-      (to, from) match {
-        case (T_Array(toType, _), T_Array(fromType, _)) =>
-          isQuestionableCoercion(toType, fromType)
-        case (T_Map(toKey, toValue), T_Map(fromKey, fromValue)) =>
-          isQuestionableCoercion(toKey, fromKey) || isQuestionableCoercion(toValue, fromValue)
-        case (T_Pair(toLeft, toRight), T_Pair(fromLeft, fromRight)) =>
-          isQuestionableCoercion(toLeft, fromLeft) || isQuestionableCoercion(toRight, fromRight)
-        case (T_String, T_String) => false
-        case (T_String, _)        => true
-        case _                    => false
-      }
-    }
-
     override def visitExpression(ctx: VisitorContext[Expr]): Unit = {
       // File-to-String coercions are normal in tasks, but flagged at the workflow level.
-      val isInWorkflow = ctx.getParentExecutable match {
-        case Some(_: Workflow) => true
-        case _                 => false
+      val allowedCoercions: Set[T] = ctx.getParentExecutable match {
+        case Some(_: Workflow) => Set(T_String, T_Any)
+        case _                 => Set(T_String, T_File, T_Directory, T_Any)
       }
-      if (isInWorkflow) {
-        // if this expression is the rhs of a declaration, check that it is coercible to the lhs type
-        ctx.getParent.element match {
-          case Declaration(_, wdlType, expr, _)
-              if expr.isDefined && isQuestionableCoercion(wdlType, expr.get.wdlType) =>
-            addEvent(ctx.element)
-          case _ => ()
+
+      def isQuestionableCoercion(toType: T, fromType: T): Boolean = {
+        (toType, fromType) match {
+          case (T_Array(toType, _), T_Array(fromType, _)) =>
+            isQuestionableCoercion(toType, fromType)
+          case (T_Map(toKey, toValue), T_Map(fromKey, fromValue)) =>
+            isQuestionableCoercion(toKey, fromKey) || isQuestionableCoercion(toValue, fromValue)
+          case (T_Pair(toLeft, toRight), T_Pair(fromLeft, fromRight)) =>
+            isQuestionableCoercion(toLeft, fromLeft) || isQuestionableCoercion(toRight, fromRight)
+          case (T_String, from) => !allowedCoercions.contains(from)
+          case _                => false
         }
-        // check compatible arguments for operations that take multiple string arguments
-        ctx.element match {
-          case ExprAdd(a, b, _, _) if ctx.findParent[ExprCompoundString].isEmpty =>
-            // if either a or b is a non-literal string type while the other is a non-string type
-            if (!Vector(a, b).exists(_.isInstanceOf[ValueString])) {
-              val wts = Vector(a, b).map(_.wdlType)
-              if (wts.contains(T_String) && !wts.forall(stringTypes.contains)) {
-                addEvent(
-                    ctx.element,
-                    Some(
-                        "string concatenation (+) has non-string argument; consider using interpolation"
-                    )
-                )
+      }
+
+      // if this expression is the rhs of a declaration, check that it is coercible to the lhs type
+      ctx.getParent.element match {
+        case Declaration(_, wdlType, expr, _)
+            if expr.isDefined && isQuestionableCoercion(wdlType, expr.get.wdlType) =>
+          addEvent(ctx.element)
+        case _ => ()
+      }
+      // check compatible arguments for operations that take multiple string arguments
+      ctx.element match {
+        case ExprAdd(a, b, _, _) if ctx.findParent[ExprCompoundString].isEmpty =>
+          if (!Vector(a, b).exists(_.isInstanceOf[ValueString])) {
+            // not within a placeholder and either a or b is a non-literal string type while
+            // the other is a non-string type
+            val wts = Vector(a, b).map(_.wdlType)
+            if (wts.contains(T_String) && !wts.forall(stringTypes.contains)) {
+              addEvent(
+                  ctx.element,
+                  Some(
+                      "string concatenation (+) has non-string argument; consider using interpolation"
+                  )
+              )
+            }
+          }
+        case ExprApply(funcName, elements, _, _) if stringFunctions.contains(funcName) =>
+          // conversion of non-string expression to string-type function parameter
+          val sigs = stdlib.getSignatures(funcName)
+          if (sigs.length != 1) {
+            throw new RuntimeException(s"Unexpected polymorphic function ${funcName}")
+          }
+          val fromTypes = elements.map(_.wdlType)
+          sigs.head match {
+            case (toTypes, _) =>
+              toTypes.zip(fromTypes).foreach {
+                case (to, from) if isQuestionableCoercion(to, from) =>
+                  addEvent(ctx.element, Some(s"${to} argument of ${funcName}() = ${from}"))
               }
-            }
-          case ExprApply(funcName, elements, _, _) if stringFunctions.contains(funcName) =>
-            // conversion of non-string expression to string-type function parameter
-            val sigs = stdlib.getSignatures(funcName)
-            val fromTypes = elements.map(_.wdlType)
-            sigs.foreach {
-              case (toTypes, _) =>
-                toTypes.zip(fromTypes).foreach {
-                  case (to, from) if isQuestionableCoercion(to, from) => addEvent(ctx.element)
-                }
-            }
-          case ExprArray(values, _, _) =>
-            // mixed string and non-string types in array
-            val types = values.map(_.wdlType)
-            if (types.contains(T_String) && !types.forall(stringTypes.contains)) {
-              addEvent(ctx.element, Some(""))
-            }
-        }
+          }
+        case ExprArray(values, _, _) =>
+          // mixed string and non-string types in array
+          val types = values.map(_.wdlType)
+          if (types.contains(T_String) && !types.forall(stringTypes.contains)) {
+            addEvent(ctx.element, Some(""))
+          }
       }
     }
   }
 
-  val allRules: Map[String, LinterTstRuleApplySig] = Map()
+  val allRules: Map[String, LinterTstRuleApplySig] = Map(
+      "T001" -> StringCoercionRule.apply
+  )
 }
