36a63863a87aea0c7e76601439c3453b1b9c01e6
jdidion <github@didion.net>
2020-05-27 09:28:06 -0400

    Fix clone-corpora script

--- src/test/resources/clone-corpora.sh
+++ src/test/resources/clone-corpora.sh
@@ -1,3 +1,3 @@
 rm -Rf corpora
 mkdir corpora
-jq '.corpora | .[] | "cd corpora && git clone \(.url) && && cd \(.url | split("/")[4] | split(".")[0]) && git checkout \(.tag)"' corpora_repos.json | xargs
+jq '.corpora | .[] | "cd corpora && git clone \(.url) && cd \(.url | split("/")[4] | split(".")[0]) && git checkout \(.tag)"' corpora_repos.json | xargs -i -t bash -c "{}"
