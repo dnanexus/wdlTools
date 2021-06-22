538d663d60925c241e1b77e179a20fcb374a2d12
jdidion <github@didion.net>
2021-06-22 09:09:06 -0700

    add script clone test workflow repos

---
+++ src/test/resources/clone-corpora.sh
@@ -0,0 +1,4 @@
+#!/usr/local/env bash
+rm -Rf corpora
+mkdir corpora
+jq '.corpora | .[] | "cd corpora && repo=\(.url | split("/")[4] | split(".")[0]) && if [ ! -d $repo ] ; then git clone \(.url) && cd $repo && git checkout \(.tag) ; fi"' corpora_repos.json | xargs -i -t bash -c "{}"
