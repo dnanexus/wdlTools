a0c99a3ea5b8576165ec574a5b4dd9ed4f0fc41b
jdidion <github@didion.net>
2021-06-25 13:44:39 -0700

    fix script

--- src/test/resources/clone-corpora.sh
+++ src/test/resources/clone-corpora.sh
@@ -1,4 +1,4 @@
-#!/usr/local/env bash
+#!/bin/bash
 rm -Rf corpora
 mkdir corpora
 jq '.corpora | .[] | "cd corpora && repo=\(.url | split("/")[4] | split(".")[0]) && if [ ! -d $repo ] ; then git clone --recurse-submodules \(.url) && cd $repo && git checkout \(.tag) ; fi"' corpora_repos.json | xargs -i -t bash -c "{}"
