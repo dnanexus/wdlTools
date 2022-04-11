3e00f9f03419a161cbf55ccf32f26f13969b02de
Yuxin Shi <yshi@dnanexus.com>
2022-04-11 15:24:56 -0700

    add io test to coerce object to struct

--- src/test/scala/wdlTools/exec/ExecTest.scala
+++ src/test/scala/wdlTools/exec/ExecTest.scala
@@ -10,6 +10,7 @@ import wdlTools.syntax.{Parsers, WdlVersion}
 import wdlTools.types.{TypeCheckingRegime, TypeInfer, TypedAbstractSyntax => TAT}
 
 import java.nio.file.{Path, Paths}
+import scala.collection.immutable.SeqMap
 
 class ExecTest extends AnyFlatSpec with Matchers with Inside {
   private val execDir = Paths.get(getClass.getResource("/exec").getPath)
@@ -45,5 +46,46 @@ class ExecTest extends AnyFlatSpec with Matchers with Inside {
                                               ignoreDefaultEvalError = false,
                                               nullCollectionAsEmpty = true)
     values("row") shouldBe V_Pair(V_String("str1"), V_Pair(V_String("str2"), V_String("str3")))
+  }
+
+  it should "evaluate task input struct with optional element" in {
+    val doc = parseAndTypeCheck(execDir.resolve("task_input_struct_optional_element.wdl"))
+    // val wf = doc.workflow.getOrElse(
+    //     None
+    // )
+    val task = doc.elements.collectFirst({ case t: TAT.Task => t }).get
+    val inputValues = Map(
+        "database" -> V_Object(SeqMap("num_rows" -> V_Int(5), "num_rows_extra" -> V_Int(5)))
+    )
+    val values = InputOutput.inputsFromValues(task.name,
+                                              task.inputs,
+                                              inputValues,
+                                              evaluator,
+                                              ignoreDefaultEvalError = false,
+                                              nullCollectionAsEmpty = true)
+    values("database") shouldBe V_Struct("MyStructDB",
+                                         "num_columns" -> V_Null,
+                                         "num_rows" -> V_Int(5))
+  }
+
+  it should "evaluate workflow input struct with optional element" in {
+    val doc = parseAndTypeCheck(execDir.resolve("workflow_input_struct_optional_element.wdl"))
+    val wf = doc.workflow.getOrElse(
+        throw new ExecException("expected workflow")
+    )
+    val inputValues = Map(
+        "database" -> V_Object(SeqMap("num_rows" -> V_Int(5), "num_rows_extra" -> V_Int(5)))
+    )
+    val values = InputOutput.inputsFromValues(wf.name,
+                                              wf.inputs,
+                                              inputValues,
+                                              evaluator,
+                                              ignoreDefaultEvalError = false,
+                                              nullCollectionAsEmpty = true)
+    values("database") shouldBe V_Struct("MyStructDB",
+                                         "database_name" -> V_Null,
+                                         "num_columns" -> V_Null,
+                                         "num_rows" -> V_Int(5),
+                                         "value_types" -> V_Null)
   }
 }
